313f10efc513f58c1cb7b2e26c9ad161af786683
Vladimirs Tanciks <vtanciks@eisgroup.com>
2024-03-26 17:10:23 +0200

    Fix issue with class collision

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
-import { StylesProvider, createGenerateClassName } from "@material-ui/core";
+import { StylesProvider, createGenerateClassName } from "@material-ui/styles";
 
 import MarketingApp from "./components/MarketingApp";
 import Header from "./components/Header";
