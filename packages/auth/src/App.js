0149a1c1dab30ac9c99daded433d062ee28779c3
Vladimirs Tanciks <vtanciks@eisgroup.com>
2024-04-03 13:40:09 +0300

    Fix casing issue

--- packages/auth/src/App.js
+++ packages/auth/src/App.js
@@ -2,8 +2,8 @@ import React from "react";
 import { Router, Route, Switch } from "react-router-dom";
 import { StylesProvider, createGenerateClassName } from "@material-ui/styles";
 
-import SignIn from "./components/SignIn";
-import SignUp from "./components/SignUp";
+import SignIn from "./components/Signin";
+import SignUp from "./components/Signup";
 
 const generateClassName = createGenerateClassName({
   productionPrefix: "au",
